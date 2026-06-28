997c7ed8089d633961a60409f37c6a2429910219
ahmed-hadaka <ahmedhadaka3@gmail.com>
2026-06-28 17:21:50 +0300

    upload backend for deployment

---
+++ src/main/java/com/java/backend/configuration/WebClientConfig.java
@@ -0,0 +1,19 @@
+package com.java.backend.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Value("${PREDICTION_API}")
+    private String predictionAPIURL;
+    @Bean
+    public WebClient predictionWebClient() {
+        return WebClient.builder().baseUrl(predictionAPIURL).defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
+    }
+
+}
